0397c548ff9464130af02fa3822d6f7170d3104b
Cameron Smith <smithc11@rpi.edu>
2022-01-06 17:06:39 -0500

    failing attempt to write vectors

--- redev.cpp
+++ redev.cpp
@@ -1,6 +1,7 @@
 #include <redev.h>
 #include <cassert>
 #include "redev_git_version.h"
+#include "redev_types.h"
 
 namespace {
   void begin_func() {
@@ -25,27 +26,54 @@ namespace redev {
   void Redev::Setup(std::vector<int>& ranks, std::vector<double>& cuts) {
     begin_func();
     io = adios.DeclareIO("rendezvous"); //this will likely change
-    std::string varName = "redev git hash";
-    std::string bpName = "redev_version.bp";
-    //rendezvous app writes the version it has and others read
+    const auto hashVarName = "redev git hash";
+    const auto bpName = "rendevous.bp";
+    const auto mode = isRendezvous ? adios2::Mode::Write : adios2::Mode::Read;
+    auto eng = io.Open(bpName, mode);
+    //rendezvous app writes the version it has and other apps read
+    eng.BeginStep();
     if(isRendezvous) {
-      auto writer = io.Open(bpName, adios2::Mode::Write);
-      auto varVersion = io.DefineVariable<std::string>(varName);
+      auto varVersion = io.DefineVariable<std::string>(hashVarName);
       if(!rank)
-        writer.Put(varVersion, std::string(redevGitHash));
-      writer.Close();
+        eng.Put(varVersion, std::string(redevGitHash));
     }
     else {
-      auto reader = io.Open(bpName, adios2::Mode::Read);
-      auto varVersion = io.InquireVariable<std::string>(varName);
+      auto varVersion = io.InquireVariable<std::string>(hashVarName);
       std::string inHash;
       if(varVersion && !rank) {
-        reader.Get(varVersion, inHash);
+        eng.Get(varVersion, inHash);
         std::cout << "inHash " << inHash << "\n";
         assert(inHash == redevGitHash);
       }
-      reader.Close();
     }
+    eng.EndStep();
+    eng.BeginStep();
+    //rendezvous app writes the partition vector and other apps read
+    auto ranksVarName = "partition vector ranks";
+    auto cutsVarName = "partition vector cuts";
+    if(isRendezvous && !rank) {
+      const auto len = ranks.size();
+      auto ranksVar = io.DefineVariable<redev::LO>(ranksVarName,{},{},{len});
+      auto cutsVar = io.DefineVariable<redev::Real>(cutsVarName,{},{},{len-1});
+      eng.Put(ranksVar, ranks.data());
+      eng.Put(cutsVar, cuts.data());
+    }
+    else { //all ranks need the partition vector; read on rank 0 then broadcast
+      if(!rank) {
+        auto var = io.InquireVariable<redev::LO>(ranksVarName);
+        auto ranksVar = io.InquireVariable<redev::LO>(ranksVarName);
+        auto cutsVar = io.InquireVariable<redev::Real>(cutsVarName);
+        assert(ranksVar && cutsVar);
+        const auto shp = ranksVar.Shape();
+        std::cerr << "len " << shp.size() << "\n"; //size is zero here...
+        //std::vector<redev::LO> inRanks(len);
+        //std::vector<redev::Real> inCuts(len-1);
+        //eng.Get(ranksVar, inRanks.data());
+        //eng.Get(cutsVar, inCuts.data());
+      }
+    }
+    eng.EndStep();
+    eng.Close();
     end_func();
   }
 }
