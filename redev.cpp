a3911787c765b676873d5a66a3b52c08dfa10039
Cameron Smith <smithc11@rpi.edu>
2022-01-07 10:35:23 -0500

    broadcast

--- redev.cpp
+++ redev.cpp
@@ -1,5 +1,6 @@
 #include <redev.h>
 #include <cassert>
+#include <type_traits> // is_same
 #include "redev_git_version.h"
 
 namespace {
@@ -7,11 +8,35 @@ namespace {
   }
   void end_func() {
   }
+
+  //TODO I think there is a cleaner way
+  template<class T>
+  MPI_Datatype getMpiType(T) {
+    MPI_Datatype mpitype;
+    //determine the type based on what is being sent
+    if( std::is_same<T, redev::Real>::value ) {
+      mpitype = MPI_DOUBLE;
+    } else if ( std::is_same<T, redev::CV>::value ) {
+      //https://www.mpi-forum.org/docs/mpi-3.1/mpi31-report/node48.htm
+      mpitype = MPI_CXX_DOUBLE_COMPLEX;
+    } else if ( std::is_same<T, redev::GO>::value ) {
+      mpitype = MPI_UNSIGNED_LONG;
+    } else if ( std::is_same<T, redev::LO>::value ) {
+      mpitype = MPI_INT;
+    } else {
+      assert(false);
+      fprintf(stderr, "Unknown type in %s... exiting\n", __func__);
+      exit(EXIT_FAILURE);
+    }
+    return mpitype;
+  }
 }
 
+
+
 namespace redev {
-  Redev::Redev(MPI_Comm comm, bool isRendezvous_)
-    : adios(comm), isRendezvous(isRendezvous_) {
+  Redev::Redev(MPI_Comm comm_, bool isRendezvous_)
+    : comm(comm_), adios(comm), isRendezvous(isRendezvous_) {
     begin_func();
     int isInitialized = 0;
     MPI_Initialized(&isInitialized);
@@ -51,6 +76,8 @@ namespace redev {
     //rendezvous app writes the partition vector and other apps read
     auto ranksVarName = "partition vector ranks";
     auto cutsVarName = "partition vector cuts";
+    std::vector<redev::LO> inRanks;
+    std::vector<redev::Real> inCuts;
     if(isRendezvous && !rank) {
       const auto len = ranks.size();
       std::cout << "writing " << len << "\n";
@@ -65,20 +92,34 @@ namespace redev {
         auto ranksVar = io.InquireVariable<redev::LO>(ranksVarName);
         auto cutsVar = io.InquireVariable<redev::Real>(cutsVarName);
         assert(ranksVar && cutsVar);
+
         auto blocksInfo = eng.BlocksInfo(ranksVar,step);
         assert(blocksInfo.size()==1);
-        std::vector<redev::LO> inRanks;
-        std::vector<redev::Real> inCuts;
         ranksVar.SetBlockSelection(blocksInfo[0].BlockID);
         eng.Get(ranksVar, inRanks);
+
+        blocksInfo = eng.BlocksInfo(ranksVar,step);
+        assert(blocksInfo.size()==1);
+        ranksVar.SetBlockSelection(blocksInfo[0].BlockID);
         eng.Get(cutsVar, inCuts);
+
         eng.PerformGets(); //default read mode is deferred
+        //TODO check if ranks and cuts are valid before the asserts
         assert(inRanks == ranks);
         assert(inCuts == cuts);
       }
     }
     eng.EndStep();
     eng.Close();
+    Broadcast(inRanks.data(), inRanks.size(), 0);
+    end_func();
+  }
+
+  template<typename T>
+  void Redev::Broadcast(T* data, int count, int root) {
+    begin_func();
+    auto type = getMpiType(T());
+    MPI_Bcast(data, count, type, root, comm);
     end_func();
   }
 }
--- redev.h
+++ redev.h
@@ -9,8 +9,11 @@ class Redev {
   public:
     Redev(MPI_Comm comm, bool isRendezvous=false);
     void Setup(std::vector<int>& ranks, std::vector<double>& cuts);
+    template<typename T>
+    void Broadcast(T* data, int count, int root);
   private:
     bool isRendezvous;
+    MPI_Comm comm;
     adios2::ADIOS adios;
     adios2::IO io;
     int rank;
