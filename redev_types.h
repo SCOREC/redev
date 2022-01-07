cf15822599b82687491d5794bdefdb466bc32d5a
Cameron Smith <smithc11@rpi.edu>
2022-01-07 10:34:21 -0500

    add complex type

--- redev_types.h
+++ redev_types.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <cstdint>
+#include <complex>
 
 namespace redev {
 typedef std::int32_t LO;
 typedef std::int64_t GO;
 typedef double Real;
+typedef std::complex<double> CV;
 }
