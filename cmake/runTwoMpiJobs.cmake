ce57091d580a8c54315460f740cb64c46368f9f0
Cameron Smith <smithc11@rpi.edu>
2022-01-10 11:13:18 -0500

    run two mpi commands concurrently

---
+++ cmake/runTwoMpiJobs.cmake
@@ -0,0 +1,21 @@
+cmake_minimum_required(VERSION 3.15.0...3.21.0)
+macro(mpi_test PROCS1 EXE1 ARGS1 PROCS2 EXE2 ARGS2)
+  execute_process(
+    RESULT_VARIABLE result
+    OUTPUT_VARIABLE stdout
+    ERROR_VARIABLE stderr
+    COMMAND_ECHO STDOUT
+    COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${PROCS1}
+            ${VALGRIND} ${VALGRIND_ARGS}
+            ${EXE1} ${ARGS1}
+    COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${PROCS2}
+            ${VALGRIND} ${VALGRIND_ARGS}
+            ${EXE2} ${ARGS2}
+  )
+  message(STATUS "result ${result}")
+  if(result)
+    message(FATAL_ERROR "Error stdout ${stdout} stderr ${stderr}")
+  endif()
+endmacro(mpi_test)
+
+mpi_test(${PROCS1} ${EXE1} ${ARGS1} ${PROCS2} ${EXE2} ${ARGS2})
