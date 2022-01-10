00b5b3d7b095e73e3de9e42d93815c9af02cfce1
Cameron Smith <smithc11@rpi.edu>
2022-01-10 11:59:38 -0500

    sleep reader to wait for writer
    is there a better way?

--- test_setup.cpp
+++ test_setup.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <cassert>
 #include "redev.h"
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
 
 int main(int argc, char** argv) {
   int rank, nproc;
@@ -13,6 +15,8 @@ int main(int argc, char** argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);
   auto isRdv = atoi(argv[1]);
+  //wait for writer to open the bp file...  TODO is there a better way?
+  if(!isRdv) std::this_thread::sleep_for(std::chrono::seconds(1));
   std::cout << "comm rank " << rank << " size " << nproc << " isRdv " << isRdv << "\n";
   //dummy partition vector data
   std::vector<redev::LO> ranks = {0,1,2,3};
