94d7eff81df69a041242a75c2c14c4833d94e30d
Cameron Smith <smithc11@rpi.edu>
2022-01-12 21:46:51 -0500

    fix dual mpi test, rm bp files
    the rendezvous application needs to run first

--- cmake/runTwoMpiJobs.cmake
+++ cmake/runTwoMpiJobs.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.15.0...3.21.0)
 macro(mpi_test PROCS1 EXE1 ARGS1 PROCS2 EXE2 ARGS2)
   execute_process(
-    RESULT_VARIABLE result
+    RESULTS_VARIABLE result
     OUTPUT_VARIABLE stdout
     ERROR_VARIABLE stderr
     COMMAND_ECHO STDOUT
@@ -12,10 +12,12 @@ macro(mpi_test PROCS1 EXE1 ARGS1 PROCS2 EXE2 ARGS2)
             ${VALGRIND} ${VALGRIND_ARGS}
             ${EXE2} ${ARGS2}
   )
-  message(STATUS "result ${result}")
-  if(result)
-    message(FATAL_ERROR "Error stdout ${stdout} stderr ${stderr}")
-  endif()
+  foreach(res IN LISTS result)
+    message(STATUS "res ${res}")
+    if(res)
+      message(FATAL_ERROR "Error stdout ${stdout} stderr ${stderr}")
+    endif()
+  endforeach()
 endmacro(mpi_test)
 
 mpi_test(${PROCS1} ${EXE1} ${ARGS1} ${PROCS2} ${EXE2} ${ARGS2})
