9d0db8fdca456b64bf738bcb2795de8990912e0a
Cameron Smith <smithc11@rpi.edu>
2022-04-29 15:54:24 -0400

    ptnObj: ptn obj is destroyed and rdv has dangling ref

--- test_initPtnObjOwnership.cpp
+++ test_initPtnObjOwnership.cpp
@@ -3,13 +3,17 @@
 
 const std::string timeout="8";
 
+auto makeRedev(int dim, redev::LOs& ranks, redev::Reals& cuts, bool isRendezvous) {
+  auto ptn = redev::RCBPtn(dim,ranks,cuts);
+  return redev::Redev(MPI_COMM_WORLD,ptn,isRendezvous);
+}
+
 void client() {
   const auto dim = 1;
   auto ranks = redev::LOs(1);
   auto cuts = redev::Reals(1);
-  auto ptn = redev::RCBPtn(dim,ranks,cuts);
-  auto isRendezvous=false;
-  auto rdv = redev::Redev(MPI_COMM_WORLD,ptn,isRendezvous);
+  const bool isRendezvous = false;
+  auto rdv = makeRedev(dim, ranks, cuts, isRendezvous);
   const bool isSST = false;
   adios2::Params params{ {"Streaming", "On"}, {"OpenTimeoutSecs", timeout}};
   auto commPair = rdv.CreateAdiosClient<redev::LO>("foo",params,isSST);
@@ -19,9 +23,8 @@ void server() {
   const auto dim = 1;
   auto ranks = redev::LOs({0});
   auto cuts = redev::Reals({0});
-  auto ptn = redev::RCBPtn(dim,ranks,cuts);
-  auto isRendezvous=true;
-  auto rdv = redev::Redev(MPI_COMM_WORLD,ptn,isRendezvous);
+  const bool isRendezvous=true;
+  auto rdv = makeRedev(dim, ranks, cuts, isRendezvous);
   const bool isSST = false;
   adios2::Params params{ {"Streaming", "On"}, {"OpenTimeoutSecs", timeout}};
   auto commPair = rdv.CreateAdiosClient<redev::LO>("foo",params,isSST);
