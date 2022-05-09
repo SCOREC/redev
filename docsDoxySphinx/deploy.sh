44f3319a52add5633785943d5dd5533210ffb11c
Cameron Smith <smithc11@rpi.edu>
2022-05-09 13:08:55 -0400

    docs: docs deployment script

--- docsDoxySphinx/deploy.sh
+++ docsDoxySphinx/deploy.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-set -e
+#!/bin/bash -ex
 
 SITE=redev
 BRANCH=gh-pages
 
 if [ -d $SITE ]; then
-   cd $SITE && git checkout . && git clean -df && git pull && cd ..
+   cd $SITE && git checkout main && git clean -df && git pull && cd ..
 else
    git clone git@github.com:SCOREC/${SITE}.git
 fi || {
@@ -13,22 +12,23 @@ fi || {
    exit 1
 }
 
-if [ ! -d pyEnv ]; then
-  python3 -m venv pyEnv
-  source pyEnv/bin/activate
+pyEnv=redevDocsPyEnv
+if [ ! -d $pyEnv ]; then
+  python3 -m venv $pyEnv
+  source $pyEnv/bin/activate
   python -m pip install sphinx-rtd-theme
   python -m pip install breathe
   python -m pip install sphinx-sitemap
   python -m pip install sphinx
 else
-  source pyEnv/bin/activate
+  source $pyEnv/bin/activate
 fi
 
 cd $SITE/docsDoxySphinx
 make html
 git checkout gh-pages
-cp -r build/html docs
-git add -A docs
+cp -r build/html/* $SITE/docs
+git add -A $SITE/docs
 git commit -m "Deploying the Redev documentation"
 git push
 
