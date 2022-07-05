48412b81be4f6838299473539cafefba97d7da8b
Cameron Smith <smithc11@rpi.edu>
2022-07-05 13:42:18 -0400

    adios_comm: close the engine in dtor

--- redev_comm.h
+++ redev_comm.h
@@ -129,6 +129,18 @@ class AdiosComm : public Communicator<T> {
       : comm(comm_), recvRanks(recvRanks_), eng(eng_), io(io_), name(name_), verbose(0) {
         inMsg.knownSizes = false;
     }
+    
+    //rule of 5 en.cppreference.com/w/cpp/language/rule_of_three
+    /// destructor to close the engine
+    ~AdiosComm() {
+      eng.Close();
+    }
+    /// don't allow copy/move/assign
+    AdiosComm(const AdiosComm& other) = delete;
+    AdiosComm(AdiosComm&& other) = delete;
+    AdiosComm& operator=(const AdiosComm& other) = delete;
+    AdiosComm& operator=(AdiosComm&& other) = delete;
+
     void SetOutMessageLayout(LOs& dest_, LOs& offsets_) {
       REDEV_FUNCTION_TIMER;
       outMsg = OutMessageLayout{dest_, offsets_};
