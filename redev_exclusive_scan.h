88c7081a17576378e6f824d01957461352cbcff1
Jacob Merson <jacob@jacobmerson.com>
2023-03-09 13:32:04 -0500

    Fix exclusive_scan

--- redev_exclusive_scan.h
+++ redev_exclusive_scan.h
@@ -1,18 +1,15 @@
 #ifndef REDEV_EXCLUSIVE_SCAN_H
 #define REDEV_EXCLUSIVE_SCAN_H
 namespace redev {
-  template <typename InputIt, typename OutputIt, typename T>
-  OutputIt exclusive_scan(InputIt first, InputIt last, OutputIt d_first, T init) {
-    (*d_first) = init;
-    ++d_first;
-    T last_val = init;
-    last = std::prev(last);
-    for(auto it = first; it!=last; ++it) {
-      *d_first = last_val + *it;
-      last_val = last_val + *it;
-      ++d_first;
-    }
-  return d_first;
+template <typename InputIt, typename OutputIt, typename T>
+OutputIt exclusive_scan(InputIt first, InputIt last, OutputIt d_first, T init) {
+  while (first != last) {
+    auto v = init;
+    init = init + *first;
+    ++first;
+    *d_first++ = std::move(v);
   }
+  return d_first;
 }
+} // namespace redev
 #endif
