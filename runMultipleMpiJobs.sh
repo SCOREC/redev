fd210b6da7322ab73c3a4cad283ed9e5ff56804e
Sichao25 <sichao_y@outlook.com>
2025-10-27 17:26:11 -0400

    config MPIEXEC_PREFLAGS

--- runMultipleMpiJobs.sh
+++ runMultipleMpiJobs.sh
@@ -1,20 +1,28 @@
 #!/bin/bash -x
-[[ $# != 10 && $# != 14 && $# != 20 ]] && \
-  echo "Usage: <run command> <process flag> <name0> <procs0> <exe0> <args0> ... <name2> <procs2> <exe2> <args2>" && \
+if [[ $# == 10 || $# == 14 || $# == 20 ]]; then
+  # Old format: no preflags
+  runCmd=${1}
+  numProcsFlag=${2}
+  preFlags=""
+  shift 2
+elif [[ $# == 11 || $# == 15 || $# == 21 ]]; then
+  # New format: with preflags
+  runCmd=${1}
+  preFlags=${2}
+  numProcsFlag=${3}
+  shift 3
+else
+  echo "Usage: <run command> <process flag> <preflags> <name0> <procs0> <exe0> <args0> ... <name2> <procs2> <exe2> <args2>" && \
   exit 1
-runCmd=${1}
-numProcsFlag=${2}
-shift 2
+fi
 
-declare -a PIDS
-declare -a LOGS
 run() {
   local name=${1}
   local procs=${2}
   local exe=${3}
   local args=${4}
   IFS=';' read -a argsArray <<< "${args}" #split the cmake list of args
-  ${runCmd} ${numProcsFlag} ${procs} ${exe} ${argsArray[@]} &> ${name}.log &
+  ${runCmd} ${preFlags} ${numProcsFlag} ${procs} ${exe} ${argsArray[@]} &> ${name}.log &
   PIDS+=($!)
   LOGS+=(${name}.log)
 }
